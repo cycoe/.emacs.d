0ccd580bcc0e8eb2ee7aecef7301a912a9180a13
cycoe <cycoe@cycoe.cc>
2020-02-24 00:06:28 +0800

    [Optimization] add nerd commenter

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,3 +1,4 @@
+(require 'evil-leader)
 (global-evil-leader-mode)
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key
@@ -9,7 +10,23 @@
   "bf" 'counsel-switch-buffer
   "ff" 'counsel-find-file
   "fr" 'counsel-recentf
-  "e" 'eval-last-sexp)
+  "e" 'eval-last-sexp
+
+  "ci" 'evilnc-comment-or-uncomment-lines
+  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "cc" 'evilnc-copy-and-comment-lines
+  "cp" 'evilnc-comment-or-uncomment-paragraphs
+  "cr" 'comment-or-uncomment-region
+  "cv" 'evilnc-toggle-invert-comment-line-by-line
+  "."  'evilnc-copy-and-comment-operator
+  "\\" 'evilnc-comment-operator)
+
+;; Emacs key bindings
+(global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+(global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
+(global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
+(global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
 
 (use-package evil-matchit
   :ensure t
