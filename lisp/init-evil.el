7dd1fc15dbd77b00260535ada6f50e3c7af3a11b
cycoe <cycoe@cycoe.cc>
2020-02-23 23:16:19 +0800

    [Optimization] configurations about evil mode
    1. clean the useless window selecting bindings
    2. add evil-magit in configuration
    3. add C-u binding

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,20 +1,15 @@
 (global-evil-leader-mode)
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key
- ";" 'counsel-M-x
- "0" 'select-window-0
- "1" 'select-window-1
- "2" 'select-window-2
- "3" 'select-window-3
- "4" 'select-window-4
- "w/" 'split-window-right
- "w-" 'split-window-below
- "wk" 'delete-window
- "bk" 'kill-buffer
- "bf" 'counsel-switch-buffer
- "ff" 'counsel-find-file
- "fr" 'counsel-recentf
- "e" 'eval-last-sexp)
+  ";" 'counsel-M-x
+  "w/" 'split-window-right
+  "w-" 'split-window-below
+  "wk" 'delete-window
+  "bk" 'kill-buffer
+  "bf" 'counsel-switch-buffer
+  "ff" 'counsel-find-file
+  "fr" 'counsel-recentf
+  "e" 'eval-last-sexp)
 
 (use-package evil-matchit
   :ensure t
@@ -26,7 +21,30 @@
   :config
   (global-evil-surround-mode 1))
 
-(require 'evil)
-(evil-mode 1)
+;; optional: this is the evil state that evil-magit will use
+;; (setq evil-magit-state 'normal)
+;; optional: disable additional bindings for yanking text
+;; (setq evil-magit-use-y-for-yank nil)
+(require 'evil-magit)
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-C-u-scroll t)
+  (when evil-want-C-u-scroll
+    (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+    (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+    (define-key evil-motion-state-map (kbd "C-u") 'evil-scroll-up)
+    (define-key evil-insert-state-map (kbd "C-u") 'evil-scroll-up))
+  :config
+  (evil-mode 1))
+
+;; If above code errors, following code can be used
+;;(setq evil-want-C-u-scroll t)
+;;(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+;;(define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+;;(define-key evil-insert-state-map (kbd "C-u") 'evil-scroll-up)
+;;(require 'evil)
+;;(evil-mode 1)
 
 (provide 'init-evil)
