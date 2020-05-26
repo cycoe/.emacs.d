56eb0beaaa9f2501c300100e8b194a5d2ebb6a98
cycoe <cycoe@cycoe.cc>
2020-05-26 21:30:14 +0800

    Refactor latex configuration

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -9,57 +9,43 @@
 
 (use-package LaTeX
   :ensure nil
-  :hook (LaTeX-mode . (lambda ()
-                        (visual-line-mode t)
-                        (LaTeX-math-mode t)))
+  :hook
+  (LaTeX-mode . (lambda ()
+                  (visual-line-mode t)
+                  (LaTeX-math-mode t)))
 
-  :config (setq-default
-           ;; Variables for LaTeX itself
-           TeX-PDF-mode t
-           TeX-view-program-list '(("llpp" "llpp %o"))
-           TeX-view-program-selection '((output-pdf "llpp"))
-           TeX-command-default "XeLaTeX"))
+  :init
+  (setq-default
+   ;; Variables for LaTeX itself
+   TeX-PDF-mode t
+   TeX-view-program-list '(("llpp" "llpp %o"))
+   TeX-view-program-selection '((output-pdf "llpp"))
+   TeX-command-default "XeLaTeX"))
 
-;; (use-package company-auctex
-;;   :ensure t
-;;   :config
-;;   (setq-default
-;;    ;; Ask the master file every time you compile
-;;    TeX-master nil
-;;    TeX-parse-self t
-;;    TeX-auto-save t)
-;;   :hook
-;;   (LaTeX-mode . company-auctex-init))
+(use-package company-auctex
+  :ensure t
+  :defer t
+  :config
+  (setq-default
+   ;; Ask the master file every time you compile
+   TeX-master nil
+   TeX-parse-self t
+   TeX-auto-save t)
+  :hook (LaTeX-mode . company-auctex-init))
 
-(setq-default
- ;; Ask the master file every time you compile
- TeX-master nil
- TeX-parse-self t
- TeX-auto-save t)
-(add-hook 'LaTeX-mode-hook 'company-auctex-init)
+(use-package reftex
+  :ensure nil
+  :defer t
+  :config
+  (require 'reftex)
+  (setq reftex-plug-into-AUCTeX t)
+  :hook (LaTeX-mode . turn-on-reftex))
 
-;; (use-package reftex
-;;   :ensure nil
-;;   :after company
-;;   (add-to-list 'company-backends '(company-reftex-labels company-reftex-citations))
-;;   :config
-;;   (require 'reftex)
-;;   (setq reftex-plug-into-AUCTeX t)
-;;   :hook
-;;   (LaTeX-mode . turn-on-reftex))
-
-(require 'reftex)
-(setq reftex-plug-into-AUCTeX t)
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-
-;; (use-package cdlatex
-;;   :ensure nil
-;;   :hook
-;;   (LaTeX-mode . turn-on-cdlatex))
-
-(add-hook 'LaTeX-mode-hook 'turn-on-cdlatex)
+(use-package cdlatex
+  :ensure nil
+  :defer t
+  :hook (LaTeX-mode . turn-on-cdlatex))
 
 (provide 'init-latex)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init-latex.el ends here
